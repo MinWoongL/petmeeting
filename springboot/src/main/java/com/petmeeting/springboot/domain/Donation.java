7ec188203c3cbc8bfffa810db256234d7405d2ba
dbflskfl1004 <dbflskfl1004@naver.com>
2023-07-20 13:43:24 +0900

    MOD / spring legacy / Entity 추가

---
+++ springboot/src/main/java/com/petmeeting/springboot/domain/Donation.java
@@ -0,0 +1,24 @@
+package com.petmeeting.springboot.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Donation {
+
+    @Id @GeneratedValue
+    @Column(name = "donation_no")
+    private Integer donationNo;
+
+}
