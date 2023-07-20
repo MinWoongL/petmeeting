b8cecc555d0685c4811fcf1bec59f832d90ff946
dbflskfl1004 <dbflskfl1004@naver.com>
2023-07-20 17:44:58 +0900

    ADD / spring legacy / enums

---
+++ springboot/src/main/java/com/petmeeting/springboot/domain/AdoptionAvailability.java
@@ -0,0 +1,13 @@
+package com.petmeeting.springboot.domain;
+
+public enum AdoptionAvailability {
+
+    ADOPT_POSSIBLE,
+
+    ADOPT_SUCCESS,
+
+    ADOPT_IMPOSSIBLE
+    
+    // 입양가능, 입양완료, 보호종료
+    
+}
