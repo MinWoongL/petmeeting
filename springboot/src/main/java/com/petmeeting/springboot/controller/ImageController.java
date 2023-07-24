4289216628abddecb80e49a8143f4bf42def438d
dbflskfl1004 <dbflskfl1004@naver.com>
2023-07-24 17:55:22 +0900

    MOD / 1-5-3. 이미지 불러오기 / getImage 구현중

--- springboot/src/main/java/com/petmeeting/springboot/controller/ImageController.java
+++ springboot/src/main/java/com/petmeeting/springboot/controller/ImageController.java
@@ -1,16 +1,23 @@
 package com.petmeeting.springboot.controller;
 
-import com.petmeeting.springboot.service.UploadImageServiceImpl;
+import com.petmeeting.springboot.service.ImageServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping ("/api/v1/image")
@@ -21,16 +28,27 @@ public class ImageController {
     @Value("${comm.uploadPath}")
     private String uploadPath;
 
-    private final UploadImageServiceImpl imageService;
+    private final ImageServiceImpl imageService;
+
+//    @Operation(
+//            summary = "이미지 가져오기",
+//            description = "경로에 있는 이미지 파일을 불러옵니다."
+//    )
+//    @GetMapping("/{imagePath}")
+//    public ResponseEntity<MultipartFile> getImage(@PathVariable String imagePath) {
+//
+//        return null;
+//    }
 
     @Operation(
             summary = "이미지 가져오기",
             description = "경로에 있는 이미지 파일을 불러옵니다."
     )
     @GetMapping("/{imagePath}")
-    public ResponseEntity<MultipartFile> getImage(@PathVariable String imagePath) {
+    public ResponseEntity<Resource> getImage(@PathVariable String imagePath) {
+        Map<String, Object> returnMap = imageService.getImage(imagePath);
 
-        return null;
+        return new ResponseEntity<>((Resource) returnMap.get("resource"), (HttpHeaders)returnMap.get("header"), HttpStatus.OK);
     }
 
     @Operation(
--- springboot/src/main/java/com/petmeeting/springboot/service/ImageServiceImpl.java
+++ springboot/src/main/java/com/petmeeting/springboot/service/ImageServiceImpl.java
@@ -1,18 +1,26 @@
 package com.petmeeting.springboot.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
-public class UploadImageServiceImpl {
+public class ImageServiceImpl {
 
     // application.yml 에 정의된 spring.servlet.multipart.location 속성과 매핑되어 있는 값을 주입받음
     // 파일 업로드시 파일을 저장할 경로를 지정
@@ -35,5 +43,33 @@ public class UploadImageServiceImpl {
         // 업로드된 파일의 경로를 문자열로 반환
         return uploadDir.getAbsolutePath() + File.separator + newFileName;
     }
+
+    public Map<String, Object> getImage(String imagePath){
+        Resource resource = new FileSystemResource(imagePath);
+
+        if(!resource.exists()){
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "노이미지");
+        }
+
+
+
+
+        HttpHeaders header = new HttpHeaders();
+        Path filePath = null;
+        try {
+            filePath = Paths.get(imagePath);
+            header.add("Content-Type", Files.probeContentType(filePath));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Map<String, Object> returnMap = new HashMap<>();
+        returnMap.put("header", header);
+        returnMap.put("resource", resource);
+
+        return returnMap;
+    }
+
+
 
 }
