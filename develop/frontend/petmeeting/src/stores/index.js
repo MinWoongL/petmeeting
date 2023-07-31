33fb6e4f8e02dcd94a05c909f5c8a10856762d2a
MinWoongL <dlalsdnd2@gmail.com>
2023-07-31 17:11:05 +0900

    MOD/index.js/dogdata,reviewdata 추가

--- develop/frontend/petmeeting/src/stores/index.js
+++ develop/frontend/petmeeting/src/stores/index.js
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import messageReducer from './Slices/MessageSlice';
-import userReducer from './Slices/UserSlice'
+import userReducer from './Slices/UserSlice';
+import dogReducer from './Slices/DogSlice'
+import reviewReducer from './Slices/ReviewSlice'
 
 const store = configureStore({
   reducer: {
     message: messageReducer,
     user: userReducer,
-    
+    dogs: dogReducer,
+    reviews: reviewReducer
   }
 });
 
