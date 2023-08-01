273e96fdbaaafd7d9c74c3321fac1825fdc3d63d
dadada0228 <dadada0228@naver.com>
2023-08-01 17:54:35 +0900

    MOD / 5-2. 문의게시글 / 문의게시글 삭제 작업 중

--- springboot/src/main/java/com/petmeeting/springboot/dto/adoption/AdoptStatusUpdateReqDto.java
+++ springboot/src/main/java/com/petmeeting/springboot/dto/adoption/AdoptStatusUpdateReqDto.java
@@ -1,11 +1,7 @@
 package com.petmeeting.springboot.dto.adoption;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@RequiredArgsConstructor
 public class AdoptStatusUpdateReqDto {
 }
