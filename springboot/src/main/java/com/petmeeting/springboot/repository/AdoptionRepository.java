3021e7138327fd6afd499b445890074437da5b0f
yoonoi <dbddbd39@naver.com>
2023-08-02 00:14:04 +0900

    MOD / 유기견 좋아요 랭킹 / 동일순위 랜덤정렬 추가

--- springboot/src/main/java/com/petmeeting/springboot/repository/AdoptionRepository.java
+++ springboot/src/main/java/com/petmeeting/springboot/repository/AdoptionRepository.java
@@ -5,14 +5,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface AdoptionRepository extends JpaRepository<Adoption, Integer> {
 
+    @Modifying
     @Query(value = "UPDATE Adoption adoption SET adoption.adoptionStatus = '미채택' WHERE adoption.dog.dogNo = ?1")
-    Adoption updateAdoptionStatus(Integer dogNo);
+    List<Adoption> updateAdoptionStatus(Integer dogNo);
 
     @Modifying
     Integer deleteAdoptionByAdoptionNo(Integer adoptionNo);
 
 //    @Query(value = "delete from bookmark_dog where member_no = :memberNo and dog_no = :dogNo", nativeQuery = true)
+
 
 }
