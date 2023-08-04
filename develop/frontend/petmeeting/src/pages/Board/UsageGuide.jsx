b3442957412261ac76672ef4d583990ee00c9d93
dadada0228 <dadada0228@naver.com>
2023-08-04 16:39:41 +0900

    ADD / Board Nav / Board Nav 추가

---
+++ develop/frontend/petmeeting/src/pages/Board/UsageGuide.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+import React from "react";
+// import { UseSelector } from "react-redux";
+
+function UsageGuide() {
+    return (
+        <Box>
+            UsageGuide
+        </Box>
+    )
+}
+
+export default UsageGuide;
