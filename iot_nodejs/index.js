a2e336478e39127f8eb865c1b1685cbd12520012
이재홍 <h78749891@gmail.com>
2023-08-04 15:49:51 +0900

    MOD/iot-backend/index.js redis 연결

--- iot_nodejs/index.js
+++ iot_nodejs/index.js
@@ -23,7 +23,7 @@ const app = express();
 // constrants
 const PORT = process.env.PORT || 3010;
 
-app.get('/:iot_command', async (req, res) => {
+app.get('/iot/:iot_command', async (req, res) => {
     console.log('GET /' + req.params.iot_command);
     res_value = client.get('iot' + req.params.iot_command, (err, reply) => {
         console.log(reply);
