0bb45fb52c557bc66c773496f6fc8abc8817eb63
김영우 <k950112@hanmail.net>
2023-08-07 15:23:02 +0900

    ADD / ShelterDetail / 보호소 상세에서 유기견 좋아요, 찜 버튼 추가

--- develop/frontend/petmeeting/src/components/Shelter/DogDetail.jsx
+++ develop/frontend/petmeeting/src/components/Shelter/DogDetail.jsx
@@ -14,6 +14,7 @@ import ShareIcon from "@mui/icons-material/Share";
 import { config } from "../../static/config";
 import { Link } from "react-router-dom";
 import LikeButton from "../Button/DogLikeButton";
+import BookmarkButton from "../Button/DogBookmarkButton";
 
 const DogDetail = ({ shelterNo }) => {
   const [dogData, setDogData] = useState(null);
@@ -76,9 +77,10 @@ const DogDetail = ({ shelterNo }) => {
                 <CardActions disableSpacing>
                   <LikeButton dogNo={dog.dogNo} />{" "}
                   {/* LikeButton 컴포넌트 사용 */}
-                  <IconButton aria-label="share">
+                  <BookmarkButton dogNo={dog.dogNo}>
+                    {" "}
                     <ShareIcon />
-                  </IconButton>
+                  </BookmarkButton>
                 </CardActions>
               </Card>
             </Link>
