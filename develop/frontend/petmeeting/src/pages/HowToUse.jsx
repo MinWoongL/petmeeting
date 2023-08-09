3ba6746fc8171a8cb6219190ad8122c88028b5bf
dbflskfl1004 <dbflskfl1004@naver.com>
2023-08-09 17:50:50 +0900

    DEL / springboot /

---
+++ develop/frontend/petmeeting/src/pages/HowToUse.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+import { Typography, Box } from "@mui/material";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+export default function HowToUse() {
+
+
+
+    return (
+        <Box>
+            <Typography>
+                이용방법
+            </Typography>
+
+
+        </Box>
+    )
+}
+
