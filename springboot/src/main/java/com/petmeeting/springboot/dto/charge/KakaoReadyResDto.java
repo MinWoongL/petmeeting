3a5cb6cce4d08e3e2a0b40a7bc5a0e49aced8915
dadada0228 <dadada0228@naver.com>
2023-08-09 13:06:02 +0900

    MOD / ChargeController / ChargeController Bug Fix

--- springboot/src/main/java/com/petmeeting/springboot/dto/charge/KakaoReadyResDto.java
+++ springboot/src/main/java/com/petmeeting/springboot/dto/charge/KakaoReadyResDto.java
@@ -8,4 +8,11 @@ import lombok.Data;
 public class KakaoReadyResDto {
     private String tid;
     private String next_redirect_pc_url;
+
+    public KakaoReadyResDto() {}
+
+    public KakaoReadyResDto(String tid, String next_redirect_pc_url) {
+        this.tid = tid;
+        this.next_redirect_pc_url = next_redirect_pc_url;
+    }
 }
