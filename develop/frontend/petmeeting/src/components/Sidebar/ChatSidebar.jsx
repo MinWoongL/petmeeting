8a37cb9bd980c8b9726101c07383feb9664f5ac9
김영우 <k950112@hanmail.net>
2023-08-10 10:46:22 +0900

    MOD / ChatSidebar / 채팅 말풍선 구분 색상 부여

--- develop/frontend/petmeeting/src/components/Sidebar/ChatSidebar.jsx
+++ develop/frontend/petmeeting/src/components/Sidebar/ChatSidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import axios from "axios";
 import { TextField, Button, Paper, Typography } from "@mui/material";
 import { config } from "../../static/config";
@@ -6,6 +6,18 @@ function ChatSidebar({ shelterNo }) {
   const [chats, setChats] = useState([]);
   const [newChat, setNewChat] = useState("");
   const token = JSON.parse(sessionStorage.getItem("token"));
+  const chatContainerRef = useRef(null);
+
+  const scrollToBottom = () => {
+    const chatDiv = chatContainerRef.current;
+    if (chatDiv) {
+      chatDiv.scrollTop = chatDiv.scrollHeight;
+    }
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [chats]);
 
   // 채팅 목록 불러오기
   const fetchChats = () => {
@@ -52,22 +64,40 @@ function ChatSidebar({ shelterNo }) {
   return (
     <div>
       <h3>채팅</h3>
-      <div>
-        {chats.map((chat, index) => (
-          <Paper
-            key={index}
-            elevation={3}
-            style={{
-              padding: "10px",
-              margin: "10px",
-              borderRadius: "20px",
-              backgroundColor: chat.isMine ? "lightblue" : "lightgrey",
-            }}
-          >
-            <Typography variant="caption">{chat.name}</Typography>
-            <Typography variant="body1">{chat.content}</Typography>
-          </Paper>
-        ))}
+      <div
+        ref={chatContainerRef}
+        style={{
+          height: "300px", // 채팅창의 높이 설정
+          overflowY: "scroll", // 세로 스크롤 허용
+          border: "1px solid lightgrey", // 경계선 추가
+          borderRadius: "10px", // 모서리 둥글게
+          padding: "10px",
+        }}
+      >
+        {chats
+          .slice()
+          .reverse()
+          .map((chat, index) => (
+            <Paper
+              key={index}
+              elevation={3}
+              style={{
+                padding: "10px",
+                maxWidth: "66%",
+                margin:
+                  chat.userNo == shelterNo
+                    ? "10px auto 10px 10px" // 보호소일 경우 왼쪽 정렬
+                    : "10px 10px 10px auto", // 그 외 오른쪽 정렬
+                borderRadius: "20px",
+                backgroundColor:
+                  chat.userNo == shelterNo ? "lightblue" : "lightgrey",
+              }}
+            >
+              <Typography variant="caption">{chat.name}</Typography>
+              <div>{chat.userName}</div>
+              <Typography variant="body1">{chat.content}</Typography>
+            </Paper>
+          ))}
       </div>
       <TextField
         label="새 채팅"
