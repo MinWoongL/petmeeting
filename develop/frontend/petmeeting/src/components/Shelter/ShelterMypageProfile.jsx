12129b69b8bd1629d3003c63b9b191bd79215d72
MinWoongL <dlalsdnd2@gmail.com>
2023-08-10 18:00:03 +0900

    MOD/BroadCastingMain.jsx/쉘터방송목록조회 및 해당세션참가로 수정

--- develop/frontend/petmeeting/src/components/Shelter/ShelterMypageProfile.jsx
+++ develop/frontend/petmeeting/src/components/Shelter/ShelterMypageProfile.jsx
@@ -3,19 +3,43 @@ import {
   Box,
   Card,
   CardContent,
-  CardMedia,
   Typography,
   Button,
   TextField,
+  Avatar,
+  Divider,
 } from "@mui/material";
 import axios from "axios";
 
-const ProfileCard = ({ profile, onChange, onUpdate, showEditButton }) => {
+const ProfileCard = ({ profile, onChange, onUpdate, showEditButton = true }) => {
   const [isEditing, setIsEditing] = useState(false);
   const [editData, setEditData] = useState(profile);
+  const token = JSON.parse(sessionStorage.getItem("token"));
 
   const handleEdit = () => {
-    setIsEditing(!isEditing);
+    setIsEditing(true);
+  };
+
+  const handleSave = async () => {
+    try {
+      const response = await axios.put(
+        `https://i9a203.p.ssafy.io/backapi/api/v1/user`,
+        editData,
+        { headers: { AccessToken: `Bearer ${token.accessToken}` } }
+      );
+
+      if (typeof onUpdate === "function") {
+        onUpdate(response.data);
+      }
+
+      setIsEditing(false);
+    } catch (error) {
+      console.error("Failed to update profile data:", error);
+    }
+  };
+
+  const handleBroadcast = () => {
+    // 여기에 방송하기 로직을 추가하세요
   };
 
   const handleChange = (field, value) => {
@@ -25,127 +49,88 @@ const ProfileCard = ({ profile, onChange, onUpdate, showEditButton }) => {
     });
   };
 
-  const handleSave = async () => {
-    // Call the API to update the profile
-    const token = JSON.parse(sessionStorage.getItem("token"));
-    const { shelterNo, joinDate, image, ...dataToSend } = editData;
-    try {
-      console.log(dataToSend);
-      await axios.put(
-        "https://i9a203.p.ssafy.io/backapi/api/v1/user",
-        dataToSend,
-        {
-          headers: { AccessToken: `Bearer ${token.accessToken}` },
-        }
-      );
-      if (typeof onUpdate === "function") {
-        onUpdate(editData);
-      }
-
-      setIsEditing(false);
-    } catch (error) {
-      console.error("Failed to update profile data:", error);
-    }
-  };
-
-  const cardMedia = (
-    <CardMedia
-      component="img"
-      sx={{ width: 151 }}
-      image={isEditing ? editData.image : profile.image}
-      alt={isEditing ? editData.name : profile.name}
-    />
-  );
-
   return (
-    <Box sx={{ border: "1px solid black" }}>
-      <Card sx={{ display: "flex", flexDirection: "row" }}>
-        <CardContent sx={{ flex: "1 0 auto" }}>
+    <Box sx={{ padding: 2 }}>
+      <Card>
+        <CardContent>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Avatar sx={{ mr: 2 }} src={isEditing ? editData.image : profile.image} />
+              <Typography variant="h5">{isEditing ? editData.name : profile.name}</Typography>
+            </Box>
+            {showEditButton && (
+              <Button variant="outlined" color="primary" onClick={isEditing ? handleSave : handleEdit}>
+                {isEditing ? "Save" : "Edit"}
+              </Button>
+            )}
+            <Button variant="outlined" color="secondary" onClick={handleBroadcast}>
+              방송하기
+            </Button>
+          </Box>
+          <Divider sx={{ my: 2 }} />
           {isEditing ? (
             <>
-              <Box>
-                <TextField
-                  label="Name"
-                  value={editData.name}
-                  onChange={(e) => handleChange("name", e.target.value)}
-                />
-              </Box>
-              <Box>
-                <TextField
-                  label="Phone Number"
-                  value={editData.phoneNumber}
-                  onChange={(e) => handleChange("phoneNumber", e.target.value)}
-                />
-              </Box>
-              <Box>
-                <TextField
-                  label="Location"
-                  value={editData.location}
-                  onChange={(e) => handleChange("location", e.target.value)}
-                />
-              </Box>
-              <Box>
-                <TextField
-                  label="Site URL"
-                  value={editData.siteUrl}
-                  onChange={(e) => handleChange("siteUrl", e.target.value)}
-                />
-              </Box>
-              <Box>
-                <TextField
-                  label="Image URL"
-                  value={editData.image}
-                  onChange={(e) => handleChange("image", e.target.value)}
-                />
-              </Box>
-              <Box>
-                <TextField
-                  type="password"
-                  label="Password"
-                  onChange={(e) => handleChange("password", e.target.value)}
-                />
-              </Box>
+              <TextField
+                fullWidth
+                label="Name"
+                value={editData.name}
+                onChange={(e) => handleChange("name", e.target.value)}
+              />
+              <TextField
+                fullWidth
+                label="Phone Number"
+                value={editData.phoneNumber}
+                onChange={(e) => handleChange("phoneNumber", e.target.value)}
+              />
+              <TextField
+                fullWidth
+                label="Location"
+                value={editData.location}
+                onChange={(e) => handleChange("location", e.target.value)}
+              />
+              <TextField
+                fullWidth
+                label="Site URL"
+                value={editData.siteUrl}
+                onChange={(e) => handleChange("siteUrl", e.target.value)}
+              />
+              <TextField
+                fullWidth
+                label="Image URL"
+                value={editData.image}
+                onChange={(e) => handleChange("image", e.target.value)}
+              />
+              <TextField
+                fullWidth
+                type="password"
+                label="Password"
+                onChange={(e) => handleChange("password", e.target.value)}
+              />
             </>
           ) : (
             <>
-              <Typography component="div" variant="h5">
-                {profile.name}
+              <Typography variant="subtitle1" color="text.secondary" component="div">
+                임시 프로필 번호: {profile.shelterNo}
               </Typography>
-              <Typography
-                variant="subtitle1"
-                color="text.secondary"
-                component="div"
-              >
-                임시 프로필 번호: {profile.shelterNO}
-                전화번호 : {profile.phoneNumber} 전화번호
+              <Typography variant="subtitle1" color="text.secondary" component="div">
+                연락처: {profile.phoneNumber}
               </Typography>
-              <Typography
-                variant="subtitle1"
-                color="text.secondary"
-                component="div"
-              >
-                지역 : {profile.location} 이메일
+              <Typography variant="subtitle1" color="text.secondary" component="div">
+                지역: {profile.location}
               </Typography>
-              <Typography
-                variant="body2"
-                color="text.secondary"
-                component="div"
-              >
-                사이트 : {profile.siteUrl} 사이트
+              <Typography variant="subtitle1" color="text.secondary" component="div">
+                사이트: {profile.siteUrl}
               </Typography>
             </>
           )}
         </CardContent>
-        {cardMedia}
       </Card>
-      {/* <Button onClick={isEditing ? handleSave : handleEdit}>
-        {isEditing ? "Save" : "Edit"}
-      </Button> */}
-      {isEditing ? (
-        <Button onClick={handleSave}>Save</Button>
-      ) : (
-        showEditButton && <Button onClick={handleEdit}>Edit</Button> // showEditButton이 true일 때만 Edit 버튼을 표시
-      )}
     </Box>
   );
 };
