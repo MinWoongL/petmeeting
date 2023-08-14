b171d8d53d58cdcc603286b5e5eaed20976473f5
dadada0228 <dadada0228@naver.com>
2023-08-14 15:16:45 +0900

    MOD / DogService 수정 / DogService Create 수정

--- springboot/src/main/java/com/petmeeting/springboot/enums/AdoptionAvailability.java
+++ springboot/src/main/java/com/petmeeting/springboot/enums/AdoptionAvailability.java
@@ -18,9 +18,9 @@ public enum AdoptionAvailability {
     }
 
     public static AdoptionAvailability getAvailability(String value) {
-        if (value.equals("입양가능"))
+        if (value.contains("입양가능"))
             return ADOPT_POSSIBLE;
-        else if (value.equals("입양완료"))
+        else if (value.contains("입양완료"))
             return ADOPT_SUCCESS;
         else
             return ADOPT_IMPOSSIBLE;
