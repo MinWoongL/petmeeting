7831f0868672bb6a7f065017819e3563e7c6a66c
MinWoongL <dlalsdnd2@gmail.com>
2023-08-17 08:05:56 +0900

    MOD/DogDetail/보호소만 좋아요 없도록 수정

--- develop/frontend/petmeeting/src/components/Shelter/DogDetail.jsx
+++ develop/frontend/petmeeting/src/components/Shelter/DogDetail.jsx
@@ -5,9 +5,16 @@ import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ShareIcon from "@mui/icons-material/Share";
 import { config } from "../../static/config";
 import { Link } from "react-router-dom";
+import LikeButton from "../Button/DogLikeButton";
+import BookmarkButton from "../Button/DogBookmarkButton";
 
 const DogDetail = ({ shelterNo }) => {
   const [dogData, setDogData] = useState(null);
@@ -15,6 +22,10 @@ const DogDetail = ({ shelterNo }) => {
   const handleLikeClick = (reviewId, event) => {
     event.stopPropagation(); // 이벤트 전파 중단
     // 좋아요 동작 처리
+  };
+  const isUser = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    return user && user.userType === '사용자';
   };
 
   useEffect(() => {
@@ -66,6 +77,15 @@ const DogDetail = ({ shelterNo }) => {
                   </Typography>
                   {/* More fields can be added here */}
                 </CardContent>
+                {/* Additional content... */}
+                {isUser() && (
+                  <CardActions disableSpacing>
+                    <LikeButton dogNo={dog.dogNo} />
+                    <BookmarkButton dogNo={dog.dogNo}>
+                      <ShareIcon />
+                    </BookmarkButton>
+                  </CardActions>
+                )}
               </Card>
             </Link>
           ))}
